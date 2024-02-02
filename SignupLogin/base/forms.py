fe0eeb15f08d39364d19f625bb0ced013d6d5449
aashiashfak <aashiashfak@gmail.com>
2024-02-02 16:30:37 +0530

    modified form

--- SignupLogin/base/forms.py
+++ SignupLogin/base/forms.py
@@ -1,17 +1,22 @@
 # base/forms.py 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
 from django import forms
 from django.forms.widgets import PasswordInput, TextInput
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+
 
 class CreateUserForm(UserCreationForm):
-    # Additional form fields or customization can be added here if needed
+    
     class Meta:
-
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].help_text = ''  # Remove username help text
+        self.fields['password1'].help_text = ''  # Remove password1 help text
+        self.fields['password2'].help_text = ''  # Remove password2 help text
 
 class LoginForm(AuthenticationForm):
 
--- SignupLogin/base/views.py
+++ SignupLogin/base/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from . forms import CreateUserForm, LoginForm
-from django.contrib.auth import authenticate, login as auth_login
-from django.contrib.auth import authenticate, logout as auth_logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
-from .models import Products
+from . models import Products
 
 
 
@@ -16,16 +15,12 @@ def signup(request):
 
     if 'username' in request.session:
         return redirect('index') 
-
     form = CreateUserForm()
-
     if request.method == 'POST':  
         form = CreateUserForm(request.POST)
-
         if form.is_valid():
             form.save()
             return redirect('login')  
-    
     context = {'signupform': form}  
     return render(request, 'signup.html', context)
 
@@ -37,7 +32,7 @@ def userlogin(request):
     form = LoginForm()
 
     if request.method =="POST":
-        form = LoginForm(request, data=request.POST)
+        form = LoginForm(request, request.POST)
 
         if form.is_valid():
             username = request.POST['username']
@@ -46,14 +41,13 @@ def userlogin(request):
             
             if user is not None:
                 request.session['username'] = username
-
                 auth_login(request, user)
                 return redirect('index')
         
     context = {'loginform': form}
     return render(request, 'login.html',context)
 
-# @login_required(login_url='login')
+@login_required(login_url='login')
 def index(request):
     visits = int(request.COOKIES.get('visits',0))
     visits+=1
@@ -66,15 +60,9 @@ def index(request):
     response.set_cookie('visits', visits)
     return response
     
-
+@login_required(login_url='login')
 def userlogout(request):
     auth_logout(request)
-
-    return redirect('home')
-    
-    # response = HttpResponseRedirect('home.html')
-
-    
-    # response.delete_cookie('visits')
-    
-    # return response
+    response=redirect('home')
+    response.delete_cookie('visits')
+    return response
