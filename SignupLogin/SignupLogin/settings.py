3b36fe3adb14cae7c8c256012966e0a41aa4f1ee
aashiashfak <aashiashfak@gmail.com>
2024-02-28 08:40:32 +0530

    latest

--- SignupLogin/SignupLogin/settings.py
+++ SignupLogin/SignupLogin/settings.py
@@ -111,26 +111,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Adding redis catche
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django_redis.cache.RedisCache',
-#         'LOCATION': 'redis://127.0.0.1:6379/1',
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-#         }
-#     }
-# }
-
-
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Specify the Redis server location
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
 }
+
+
+
 
 
 # Internationalization
